18dd4f46ea287f7b0ee26286a3437e73b468d397
Siam-WD <abusayedalsiam2023@gmail.com>
2023-03-28 15:27:14 +0600

    all complete local storage and display cart data

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -17,12 +17,37 @@ const Shop = () => {
     
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[])
+        const savedCart = [];
+        // step 1 get id 
+        for(const id in storedCart){
+            // step 2 get the product by using id 
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+               // step 3 get quantity of the product 
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4 add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+            // step 5 set saved cart to the set cart state 
+            setCart(savedCart);
+        }
+    } , [products])
     
     const handleAddToCart = (product) =>{
+        let newCart = [];
+        const exist =cart.find(pd => pd.id === product.id);
+        if(!exist){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exist.quantity = exist.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exist]
+        }
         // cart.push(product); //evabe hobe na
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id);
     }
